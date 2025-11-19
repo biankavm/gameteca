f8b395e70d2531da9a52c292484a79e9cf7d4552
biankavm <bianka.maciel@jusbrasil.com.br>
2025-11-18 21:55:17 -0400

    feat: add game card component

--- src/components/games/card/card.tsx
+++ src/components/games/card/card.tsx
@@ -1,9 +1,37 @@
 import { Game } from '@/types/game'
+import Image from 'next/image'
+import Link from 'next/link'
+import { BiRightArrowCircle } from 'react-icons/bi'
 
 interface GameCardProps {
   game: Game
 }
 
 export function GameCard({ game }: GameCardProps) {
-  return <h1> {game.title}</h1>
+  const { id, title, image_url } = game
+
+  return (
+    <Link href={`/game/${id}`}>
+      <section className="w-full bg-slate-200 rounded-lg p-4 mb-5">
+        <div className="relative w-full h-56 hover:scale-105 transition-all duration-300">
+          <Image
+            src={image_url}
+            alt={title}
+            fill
+            quality={100}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
+            className="rounded-lg object-cover"
+          />
+        </div>
+
+        <div className="flex items-center mt-4 justify-between">
+          <p className="text-sm font-bold px-2 text-black text-ellipsis truncate whitespace-nowrap overflow-hidden">
+            {' '}
+            {title}
+          </p>
+          <BiRightArrowCircle size={24} color="#545352" />
+        </div>
+      </section>
+    </Link>
+  )
 }
--- src/components/games/list/list.tsx
+++ src/components/games/list/list.tsx
@@ -9,7 +9,7 @@ export function GamesList({ games }: GamesListProps) {
   return (
     <>
       <h2 className="text-lg font-bold mt-8 mb-5"> Jogos para conhecer </h2>
-      <section>
+      <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {games.map((game, idx) => (
           <div key={idx}>
             <GameCard game={game} />
